2a74ffe3bfa1f85b9747e76f7b3dc6ce0e016f87
Sammyt <sammy.teillet@gmail.com>
2018-11-04 10:05:38 +0100

    test(): Add first test

---
+++ src/application/article/article.controller.spec.ts
@@ -0,0 +1,28 @@
+import { Test } from '@nestjs/testing';
+import { ArticleController } from './article.controller';
+import { ArticleService } from './article.service';
+import { CommandBus, CQRSModule } from '@nestjs/cqrs';
+
+describe('Article controller', () => {
+  let articleService: ArticleService;
+  let articleController: ArticleController;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      imports: [CQRSModule],
+      providers: [ArticleService],
+      controllers: [ArticleController],
+    }).compile();
+    articleService = module.get<ArticleService>(ArticleService);
+    articleController = module.get<ArticleController>(ArticleController);
+  });
+
+  describe('findAll', () => {
+    it('should return an article', async () => {
+      const result = 'Un article';
+      jest.spyOn(articleService, 'root').mockImplementation(() => result);
+
+      expect(await articleController.root()).toBe(result);
+    });
+  });
+});
