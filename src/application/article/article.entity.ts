0fef5a481f78037aa0dfaa98b7ba5a2ed6393296
Sammyt <sammy.teillet@gmail.com>
2018-11-26 21:06:24 +0100

    feat(): Store article id in catalog

--- src/application/article/article.entity.ts
+++ src/application/article/article.entity.ts
@@ -4,10 +4,12 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  getRepository,
 } from 'typeorm';
 import { ArticlePublished } from './events/article-published';
 import { ArticleCreated } from './events/article-created.event';
 import { AggregateRoot } from '../AggregateRoot';
+import { Catalog } from '../catalog/catalog.entity';
 
 @Entity()
 export class Article extends AggregateRoot {
@@ -40,9 +42,16 @@ export class Article extends AggregateRoot {
     this.apply(articleCreatedEvent);
   }
 
-  onArticleCreated(event: ArticleCreated) {
+  async onArticleCreated(event: ArticleCreated) {
     this.id = event.id;
     this.name = event.name;
     this.content = event.content;
+    const catalogRepository = getRepository(Catalog);
+    const articleCatalog: Catalog = (await catalogRepository.findOne('article')) || {
+      entityName: 'article',
+      ids: [],
+    };
+    articleCatalog.ids.push(event.id);
+    catalogRepository.save(articleCatalog);
   }
 }
--- src/application/article/events/handlers/article-created.handler.ts
+++ src/application/article/events/handlers/article-created.handler.ts
@@ -1,9 +1,17 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { ArticleCreated } from '../article-created.event';
+import { getRepository } from 'typeorm';
+import { Catalog } from '../../../catalog/catalog.entity';
 
 @EventsHandler(ArticleCreated)
 export class ArticleCreatedHandler implements IEventHandler<ArticleCreated> {
-  handle(event: ArticleCreated) {
-    // @todo add articleId to a list of all the articles
+  async handle(event: ArticleCreated) {
+    const catalogRepository = getRepository(Catalog);
+    const articleCatalog: Catalog = (await catalogRepository.findOne('article')) || {
+      entityName: 'article',
+      ids: [],
+    };
+    articleCatalog.ids.push(event.id);
+    catalogRepository.save(articleCatalog);
   }
 }
