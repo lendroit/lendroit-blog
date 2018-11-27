0c6ba6f328faa99f44af9cbe982b7a82fa66a183
Sammyt <sammy.teillet@gmail.com>
2018-11-27 14:02:44 +0100

    fix(): Save article to get an id

--- src/application/article/commands/handlers/create-article.handler.ts
+++ src/application/article/commands/handlers/create-article.handler.ts
@@ -1,13 +1,19 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CreateArticleCommand } from '../implementations/create-article.command';
 import { Article } from '../../article.entity';
+import { getRepository } from 'typeorm';
 
 @CommandHandler(CreateArticleCommand)
 export class CreateArticleHandler implements ICommandHandler<CreateArticleCommand> {
   async execute(command: CreateArticleCommand, resolve: (value?) => void) {
     const article = new Article();
-    article.createArticle(command.newArticle.name, command.newArticle.content);
-    article.commit();
+    // TODO remove all this shit, handle the id creation myself
+    article.name = command.newArticle.name;
+    article.content = command.newArticle.content;
+    article.isPublished = false;
+    const articleWithID = await getRepository(Article).save(article);
+    articleWithID.createArticle(command.newArticle.name, command.newArticle.content);
+    articleWithID.commit();
     resolve(article);
   }
 }
